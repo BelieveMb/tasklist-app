c62aa09b1f523abd3bef0f0b603601ba8a374d7b
Dev Unbelievable <mbolokobelieve@gmail.com>
2024-02-24 09:49:07 +0100

    to create the  routes and services

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => '/', 'as' => 'tasks.'], function () {
+    Route::get('/', [TaskController::class, 'index'])->name('index');
+});
